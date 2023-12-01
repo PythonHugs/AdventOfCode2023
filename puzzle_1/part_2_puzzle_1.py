1022cd1815db8a39bea113ba4ad15ae0282956f7
Jason Schnereger <Jason.Schnereger@sony.com>
2023-12-01 14:44:56 -0800

    fixed a bug in puzzle 1 part 2 and added some comments in the larger function

--- puzzle_1/part_2_puzzle_1.py
+++ puzzle_1/part_2_puzzle_1.py
@@ -17,20 +17,26 @@ def get_calibration_value(line):
     sanitized_line = line.strip('\n')
     for char in sanitized_line:
         try:
+            # if we hit an integer, put it in the list of numbers and clear the char_stack
             number = int(char)
             numbers_in_line.append(number)
+            char_stack = []
         except ValueError:
+            # if we hit a char, add it to the stack and check the stack for valid numbers
             char_stack.append(char)
             char_string = ''.join(char_stack)
             for key in string_numbers.keys():
                 if key in char_string:
+                    # if we find a number in the char_stack, add that number to the number list
                     numbers_in_line.append(string_numbers[key])
+                    # reset the char_stack to the number we found minus the first letter to avoid duplicate finds
                     char_string = key
                     char_stack = list(char_string)
                     char_stack.pop(0)
     if len(numbers_in_line) > 1:
         calibration_value = f'{numbers_in_line[0]}{numbers_in_line[-1]}'
     else:
+        # if there is only 1 number, return it twice as first and last digit
         calibration_value = f'{numbers_in_line[0]}{numbers_in_line[0]}'
     return int(calibration_value)
 
