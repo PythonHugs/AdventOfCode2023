6a3c20753243d28f1da089ef3cd5c1b9e7c6be17
Jason Schnereger <Jason.Schnereger@sony.com>
2023-12-02 22:24:12 -0800

    added files for puzzle 3

---
+++ puzzle_3/part_2_puzzle_3.py
@@ -0,0 +1,10 @@
+""" Puzzle 2 for Puzzle 3 Advent of Code 2023 """
+# https://adventofcode.com/2023/day/3
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ puzzle_3/puzzle_3.py
@@ -0,0 +1,10 @@
+""" Puzzle 3 for Advent of Code 2023 """
+# https://adventofcode.com/2023/day/3
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
