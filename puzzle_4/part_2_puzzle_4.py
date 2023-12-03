555e5e44e6fce6e8e711aa29e707ab5b4547e558
Jason Schnereger <Jason.Schnereger@sony.com>
2023-12-03 12:04:41 -0800

    prep for puzzle 4

---
+++ puzzle_4/part_2_puzzle_4.py
@@ -0,0 +1,18 @@
+""" Puzzle 2 for Puzzle 4 Advent of Code 2023 """
+# https://adventofcode.com/2023/day/4
+
+
+def read_input(input_file):
+    with open(input_file, 'r') as file:
+        puzzle_input = []
+        for line in file:
+            puzzle_input.append(line[:-1])
+        return puzzle_input
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ puzzle_4/puzzle_4.py
@@ -0,0 +1,18 @@
+""" Puzzle 4 for Advent of Code 2023 """
+# https://adventofcode.com/2023/day/4
+
+
+def read_input(input_file):
+    with open(input_file, 'r') as file:
+        puzzle_input = []
+        for line in file:
+            puzzle_input.append(line[:-1])
+        return puzzle_input
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
