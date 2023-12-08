a0a649a7eb0d2775536a801d954bafb76d42dea8
Jason Schnereger <Jason.Schnereger@sony.com>
2023-12-07 17:55:24 -0800

    Puzzle 7 complete
    Solution for Puzzle 7.

--- puzzle_7/puzzle_7.py
+++ puzzle_7/puzzle_7.py
@@ -20,7 +20,13 @@ def parse_data(puzzle_input):
         hand, bid = data.split(' ')
         hands.append(hand)
         bids.append(int(bid))
-    return hands, bids
+    hands_data = {}
+    for i in range(len(hands)):
+        hands_data[i] = {}
+        hands_data[i]['cards'] = hands[i]
+        hands_data[i]['bid'] = bids[i]
+        hands_data[i]['type'] = count_hand(hands[i])
+    return hands_data
 
 
 def count_hand(hand):
@@ -41,9 +47,7 @@ def count_hand(hand):
     }
     for card in hand:
         key[card] += 1
-
     hand_type = ''
-
     if 5 in key.values():
         hand_type = 'five of a kind'
     elif 4 in key.values():
@@ -64,158 +68,105 @@ def count_hand(hand):
                 hand_type = 'one pair'
     else:
         hand_type = 'high card'
-
     return hand_type
 
 
-def rank_hands(hands_data_list):
-    hierarchy = [
-        'high card',
-        'one pair',
-        'two pairs',
-        'three of a kind',
-        'full house',
-        'four of a kind',
-        'five of a kind'
-    ]
-    for i in range(len(hands_data_list)):
-        hands_data_list[i]['rank'] = hierarchy.index(hands_data_list[i]['type'])
-    sorted_hands_data = sorted(hands_data_list, key=lambda h: h['rank'])
-    return sorted_hands_data
-
-
-def sort_ranked_hands(hands_data):
-    ranks = []
-    hands = []
-    for i in range(len(hands_data.keys())):
-        ranks.append([hands_data[i]['rank'], i])
-        hands.append(hands_data[i]['cards'])
-    print(ranks)
-    print(hands)
+def output_hands_data(hands_data):
+    for hands in hands_data.items():
+        print(hands)
     print('')
 
-    rank_sorted_hands = sorted(ranks)
-    return rank_sorted_hands
+
+def categorize_hands(hand_types, hands_data):
+    hand_types_dict = {hand_type: [] for hand_type in hand_types}
+    for hand_type in hand_types:
+        for hand in hands_data:
+            if hands_data[hand]['type'] == hand_type:
+                hand_types_dict[hand_type].append(hands_data[hand])
+    return hand_types_dict
 
 
-def break_ties(hands_data_list):
-    challenge = []
-    card_value = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    hierarchy = [
-        'high card',
-        'one pair',
-        'two pairs',
-        'three of a kind',
-        'full house',
-        'four of a kind',
-        'five of a kind'
-    ]
-    for i in range(len(hands_data_list) - 1):
-        challenge = []
-        try:
-            hand_1 = hands_data_list[i]
-            hand_2 = hands_data_list[i + 1]
-            # if hand_1['type'] != hand_2['type']:
-            #     if hierarchy.index(hand_1['type']) > hierarchy.index(hand_2['type']) and hand_1['rank'] == hand_2['rank']:
-            #         if hand_1['rank'] != len(hands_data_list) - 1:
-            #             hand_1['rank'] += 2
-            #             break
-            #     elif hierarchy.index(hand_1['type']) < hierarchy.index(hand_2['type']) and hand_1['rank'] == hand_2['rank']:
-            #         if hand_2['rank'] != len(hands_data_list) - 1:
-            #             hand_2['rank'] += 2
-            #             break
-            temp_hold = []
-            if hand_1['type'] == hand_2['type']:
-                for x in range(len(hand_1['cards'])):
-                    hand_1_card_value = card_value.index(hand_1['cards'][x])
-                    hand_2_card_value = card_value.index(hand_2['cards'][x])
-                    if hand_1_card_value == hand_2_card_value:
-                        continue
-                    if hand_1_card_value > hand_2_card_value:
-                        temp_hold.append(hand_1)
-                        hand_1 = hand_2
-                        hand_2 = temp_hold[0]
-
-            #         elif hand_1_card_value < hand_2_card_value:
-            #             if hand_2['rank'] != len(hands_data_list) - 1:
-            #                 hand_2['rank'] += 1
-            #                 break
-            # elif hand_1['type'] == hand_2['type']:
-            #     for x in range(len(hand_1['cards'])):
-            #         hand_1_card_value = card_value.index(hand_1['cards'][x])
-            #         hand_2_card_value = card_value.index(hand_2['cards'][x])
-            #         if hand_1_card_value == hand_2_card_value:
-            #             continue
-            #         if hand_1_card_value > hand_2_card_value and hand_1['rank'] < hand_2['rank']:
-            #             hand_1['rank'], hand_2['rank'] = hand_2['rank'], hand_1['rank']
-            #             break
-            #         elif hand_1_card_value < hand_2_card_value and hand_1['rank'] > hand_2['rank']:
-            #             hand_1['rank'], hand_2['rank'] = hand_2['rank'], hand_1['rank']
-            #             break
-
-        except IndexError:
-            pass
-
-        print(challenge)
-
-    # check_list = []
-    # for i in range(len(hands_data_list)):
-    #     check_list.append(hands_data_list[i]['rank'])
-
-    # print(check_list)
-    # print(set(check_list))
-
-    # sorted_hands_data = sorted(hands_data_list, key=lambda h: h['rank'])
-    # if i == len(hands_data_list) - 1:
-    #     return break_ties(sorted_hands_data)
-    return hands_data_list
+def translate_cards(categorized_hands):
+    for hand_type, hands in categorized_hands.items():
+        translated_cards = []
+        for hand in hands:
+            for card in hand['cards']:
+                if card == 'A':
+                    translated_cards.append(14)
+                elif card == 'K':
+                    translated_cards.append(13)
+                elif card == 'Q':
+                    translated_cards.append(12)
+                elif card == 'J':
+                    translated_cards.append(11)
+                elif card == 'T':
+                    translated_cards.append(10)
+                else:
+                    translated_cards.append(int(card))
+            categorized_hands[hand_type][categorized_hands[hand_type].index(hand)]['cards'] = translated_cards
+            translated_cards = []
 
 
-def output_hand_data(hands_data_list):
-    for i in range(len(hands_data_list)):
-        print(hands_data_list[i])
+def translate_hands(categorized_hands):
+    translated_hands = []
+    for hand_data in categorized_hands.items():
+        sorted_hand_data = sorted(hand_data[1], key=lambda x: x['cards'], reverse=True)
+        print(sorted_hand_data)
+        if sorted_hand_data:
+            translated_hands.append(sorted_hand_data)
     print('')
+    for data in translated_hands:
+        print(data)
+    print('')
+    return translated_hands
+
+
+def determine_winnings(translated_hands):
+    ranked_bids = []
+    for hand_type in translated_hands:
+        for hand in reversed(hand_type):
+            print(hand)
+            ranked_bids.append(hand['bid'])
+    print('')
+    winnings = []
+    for bid in ranked_bids:
+        bid_index = ranked_bids.index(bid)
+        earned = bid * (ranked_bids.index(bid) + 1)
+        print(bid, bid_index, earned)
+        winnings.append(earned)
+    print('')
+    return sum(winnings)
 
 
 def main():
-    puzzle_input = read_input('puzzle_7_example_input.txt')
+    puzzle_input = read_input('puzzle_7_input.txt')
     print(puzzle_input)
     print('')
 
-    hands, bids = parse_data(puzzle_input)
-    # print(hands)
-    # print(bids)
-    # print('')
+    hands_data = parse_data(puzzle_input)
+    output_hands_data(hands_data)
 
-    hands_data = {}
-    for i in range(len(hands)):
-        hands_data[i] = {}
-        hands_data[i]['cards'] = hands[i]
-        hands_data[i]['bid'] = bids[i]
-        hands_data[i]['type'] = count_hand(hands[i])
+    # split hands into types
+    hand_types = [
+        'high card',
+        'one pair',
+        'two pairs',
+        'three of a kind',
+        'full house',
+        'four of a kind',
+        'five of a kind'
+    ]
 
-    hands_data_list = []
-    for hand in hands_data.keys():
-        hands_data_list.append(hands_data[hand])
+    categorized_hands = categorize_hands(hand_types, hands_data)
+    output_hands_data(categorized_hands)
 
-    sorted_hands_data = rank_hands(hands_data_list)
-    output_hand_data(sorted_hands_data)
+    translate_cards(categorized_hands)
+    output_hands_data(categorized_hands)
 
-    final_hands_data = break_ties(sorted_hands_data)
-    output_hand_data(final_hands_data)
+    translated_hands = translate_hands(categorized_hands)
 
-    # rank_sorted_hands = sort_ranked_hands(hands_data)
-    # print(rank_sorted_hands)
-    # print('')
-    #
-    # ranked_hands = break_ties(rank_sorted_hands, hands)
-    # print(ranked_hands)
-    # print('')
-
-    # for i in range(len(ranked_hands) - 1, -1, -1):
-    #     print(hands[i])
-
-
+    total_winnings = determine_winnings(translated_hands)
+    print(total_winnings)
 
 
 if __name__ == '__main__':
